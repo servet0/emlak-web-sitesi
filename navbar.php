abe268725c3a7cab9758be46764eb05d5f04b830
servet0 <servetacar540@gmail.com>
2024-12-20 17:19:18 +0300

    Css ve javascript dosyaları eklendi. Anasayfa, baglan, navbar php dosyaları oluşturuldu

---
+++ navbar.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+    <div class="container">
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
+        aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <a class="navbar-brand text-brand" href="index.html">Es Emlak</span></a>
+      <button type="button" class="btn btn-link nav-search navbar-toggle-box-collapse d-md-none" data-toggle="collapse"
+        data-target="#navbarTogglerDemo01" aria-expanded="false">
+        <span class="fa fa-search" aria-hidden="true"></span>
+      </button>
+      <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="index.html">Anasayfa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">Hakkında</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="property-grid.html">Kiralık</a>
+          </li>
+         
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">İletişim</a>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+  </nav>
