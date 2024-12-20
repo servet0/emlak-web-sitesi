51972ddb6d3190b6869aa3ebdae1a00a1ec0e468
servet0 <servetacar540@gmail.com>
2024-12-20 17:56:36 +0300

    Carousel Star kısmı düzenlendi ve veritabanına bağlanıp veri çekildi

--- anasayfa.php
+++ anasayfa.php
@@ -41,79 +41,50 @@
   <!--/ Nav End /-->
 
   <!--/ Carousel Star /-->
-  <div class="intro intro-carousel">
+  <?php
+include 'baglan.php';
+
+// Son 4 veriyi çek
+$query = "SELECT * FROM kiralik_daireler ORDER BY id DESC LIMIT 4";
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+    die("Sorgu başarısız: " . mysqli_error($conn));
+}
+?>
+<div class="intro intro-carousel">
     <div id="carousel" class="owl-carousel owl-theme">
-      <div class="carousel-item-a intro-item bg-image" style="background-image: url(img/slide-1.jpg)">
-        <div class="overlay overlay-a"></div>
-        <div class="intro-content display-table">
-          <div class="table-cell">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8">
-                  <div class="intro-body">
-                    <p class="intro-title-top">Doral, Florida
-                      <br> 78345</p>
-                    <h1 class="intro-title mb-4">
-                      <span class="color-b">204 </span> Mount
-                      <br> Olive Road Two</h1>
-                    <p class="intro-subtitle intro-price">
-                      <a href="#"><span class="price-a">rent | $ 12.000</span></a>
-                    </p>
-                  </div>
+        <?php while ($row = mysqli_fetch_assoc($result)): ?>
+            <div class="carousel-item-a intro-item bg-image" style="background-image: url('<?php echo $row['resim']; ?>')">
+                <div class="overlay overlay-a"></div>
+                <div class="intro-content display-table">
+                    <div class="table-cell">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-8">
+                                    <div class="intro-body">
+                                        <p class="intro-title-top">
+                                            <?php echo $row['konum']; ?>
+                                            <br> Genişlik: <?php echo $row['alan']; ?> m²
+                                        </p>
+                                        <h1 class="intro-title mb-4">
+                                            <span class="color-b"> <?php echo $row['baslik']; ?></span>
+                                        </h1>
+                                        <p class="intro-subtitle intro-price">
+                                            <a href="#"><span class="price-a">Kira Fiyatı | <?php echo $row['fiyat']; ?> TL</span></a>
+                                        </p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item-a intro-item bg-image" style="background-image: url(img/slide-2.jpg)">
-        <div class="overlay overlay-a"></div>
-        <div class="intro-content display-table">
-          <div class="table-cell">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8">
-                  <div class="intro-body">
-                    <p class="intro-title-top">Doral, Florida
-                      <br> 78345</p>
-                    <h1 class="intro-title mb-4">
-                      <span class="color-b">204 </span> Rino
-                      <br> Venda Road Five</h1>
-                    <p class="intro-subtitle intro-price">
-                      <a href="#"><span class="price-a">rent | $ 12.000</span></a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item-a intro-item bg-image" style="background-image: url(img/slide-3.jpg)">
-        <div class="overlay overlay-a"></div>
-        <div class="intro-content display-table">
-          <div class="table-cell">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8">
-                  <div class="intro-body">
-                    <p class="intro-title-top">Doral, Florida
-                      <br> 78345</p>
-                    <h1 class="intro-title mb-4">
-                      <span class="color-b">204 </span> Alira
-                      <br> Roan Road One</h1>
-                    <p class="intro-subtitle intro-price">
-                      <a href="#"><span class="price-a">rent | $ 12.000</span></a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        <?php endwhile; ?>
     </div>
-  </div>
+</div>
+
   <!--/ Carousel end /-->
 
   <!--/ Services Star /-->
